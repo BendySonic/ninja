415ff37f03f9f46e54979f74a25324291e8c3e3c
BendySonic <nurbaevdaniar0@gmail.com>
2025-09-15 21:13:36 +0500

    Добавить "прыжок койота", изменить карту, добавить двойной прыжок

--- src/player.gd
+++ src/player.gd
@@ -4,15 +4,24 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 
+@onready var on_floor: Area2D = get_node("OnFloor")
+
+var has_second_jump := true
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	if is_on_floor() or is_on_floorc():
+		has_second_jump = true
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and (is_on_floor() or is_on_floorc()):
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("jump") and has_second_jump:
+		velocity.y = JUMP_VELOCITY
+		has_second_jump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,3 +32,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func is_on_floorc() -> bool:
+	return on_floor.has_overlapping_bodies()
