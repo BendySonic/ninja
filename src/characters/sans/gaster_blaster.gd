316f4355d2708cfc4c9daa41827dd7411ac7d1d8
hopesofbuzzy <nurbaevdaniar0@gmail.com>
2025-09-25 19:44:37 +0500

    Геймплей: (почти) рабочая способность
    Первая способность - гастер-бластер

--- src/characters/sans/gaster_blaster.gd
+++ src/characters/sans/gaster_blaster.gd
@@ -1,12 +1,20 @@
 extends Area2D
 
 @onready var raycast: RayCast2D = get_node("RayCast2D")
+@onready var laser: MeshInstance2D = get_node("Laser")
+@onready var animation: AnimationPlayer = get_node("AnimationPlayer")
 var collision_point := Vector2(0, 0)
 
 
 func _physics_process(delta: float) -> void:
 	collision_point = raycast.get_collision_point()
-	queue_redraw()
+	var cpl
+	if collision_point == Vector2(0, 0):
+		cpl = 6000
+	else:
+		cpl = (global_position - collision_point).length()
+	laser.mesh.size.x = cpl*2
+	laser.position.x = cpl/2
 
-func _draw() -> void:
-	draw_line(Vector2(0, 0), to_local(collision_point), Color.WHITE, 15.0)
+func _ready() -> void:
+	animation.play("shoot")
